a721a5da39e4986300ddc5ad9569cbdce3f77275
su-haas <uday0003@algonquinlive.com>
2025-07-07 01:54:00 -0400

    Editef heading of comment in view_cc.py: Activity level & corresponding descriptions

--- view/view_cc.py
+++ view/view_cc.py
@@ -189,7 +189,7 @@ class CalorieCalculatorView:
         activityLevel_label.grid(row=6, column=0, sticky=tk.W, pady= 5)
         # Activity level dropdown menu
         '''
-        Activity levels and their corresponding meaning.
+        NOTE: Activity levels and their corresponding meaning.
             'Sedentary: Minimal or no exercise in a week.',
             'Lightly Active: Exercise lightly one to three days a week.', 
             'Moderately Active: Exercise moderately three to five days a week.', 
